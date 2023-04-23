103334cbf87324938060e89e59a87b0d2e18d1e4
HoneyGitHub123 <honeytom01@gmail.com>
2023-04-23 15:29:15 -0700

    Committing after updates(homework+code review)

---
+++ airport1/AirportManagement.java
@@ -0,0 +1,18 @@
+package homework.airport1;
+
+/*Used polymorphism with at least one abstract class
+Used 3 Protected variables*/
+public abstract class AirportManagement {
+    protected String airportId;
+    protected String airportName;
+    protected String airportLocation;
+
+    public AirportManagement(String airportName) {
+        System.out.println("Welcome to" + " " + airportName);
+    }
+
+    //Abstract method that must be implemented in the child class
+    public abstract void getAirportDetails(String airportId, String airportName,
+                                           String airportLocation);
+
+}
---
+++ airport1/Employees.java
@@ -0,0 +1,18 @@
+package homework.airport1;
+
+import homework.airport.Salary;
+
+/*Used polymorphism with at least one abstract class
+Used 2 protected variables
+Added interface to the abstract class*/
+public abstract class Employees implements Salary {
+    protected String employeeName;
+    protected String employeeId;
+
+    public Employees() {
+    }
+
+    //Abstract method that must  be implemented in the child class
+    public abstract void getEmployeeDetails();
+}
+
